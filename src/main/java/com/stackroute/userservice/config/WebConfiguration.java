4c884d512d2898caa317d48f9c69e80501ecc704
sauravsinha137 <k123saurav@gmail.com>
2019-10-17 20:57:34 +0530

    added properties files for dev and prod

--- src/main/java/com/stackroute/userservice/config/WebConfiguration.java
+++ src/main/java/com/stackroute/userservice/config/WebConfiguration.java
@@ -1,13 +1,20 @@
 package com.stackroute.userservice.config;
 
-
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 
 @Configuration
+@Profile("prod")
 public class WebConfiguration {
 
-
+    @Bean
+    ServletRegistrationBean h2ServletRegistraion() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return  registrationBean;
+    }
 }
